42efae25ea43d58af6331b24c779d63224666165
jueun275 <52851721+jueun275@users.noreply.github.com>
2021-12-29 19:49:38 +0900

    구글 로그인 기능 구현
    Spring Security, OAuth2  이용

--- src/main/java/com/jueun/toyproject/web/IndexController.java
+++ src/main/java/com/jueun/toyproject/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jueun.toyproject.web;
 
+import com.jueun.toyproject.config.auth.dto.SessionUser;
 import com.jueun.toyproject.service.PostService;
 import com.jueun.toyproject.web.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("post", postService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
