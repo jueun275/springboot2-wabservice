09affdd2eb94f13e2b3b406f53893f1c54ce76c8
jueun275 <52851721+jueun275@users.noreply.github.com>
2022-01-04 23:43:17 +0900

    세션값 가져오는 어노테이션 생성 및 적용

--- src/main/java/com/jueun/toyproject/web/IndexController.java
+++ src/main/java/com/jueun/toyproject/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jueun.toyproject.web;
 
+import com.jueun.toyproject.config.auth.LoginUser;
 import com.jueun.toyproject.config.auth.dto.SessionUser;
 import com.jueun.toyproject.service.PostService;
 import com.jueun.toyproject.web.dto.PostResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("post", postService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
